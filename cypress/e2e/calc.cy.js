59e900511f343d9a7c260be9afeddf2fdc4b885b
Maxim Litvak <maxim.litvak@gmail.com>
2024-10-04 15:31:42 +0200

    [#32] cypress tests for different params added

--- cypress/e2e/calc.cy.js
+++ cypress/e2e/calc.cy.js
@@ -3,6 +3,8 @@ describe('Test calculation with user input', () => {
       // Visit the correct page
     cy.visit('/cf');
 
+    // -----------------
+    // Test 1: calculate the interest rate
     // Enter 100000 in P0Display-1
     cy.get('#P0Display-1').clear().type('100000');
 
@@ -13,28 +15,71 @@ describe('Test calculation with user input', () => {
     cy.get('#search-btn-1').click();
 
     // Wait for calculations to complete
-    cy.wait(1000);
+    cy.wait(300);
 
     // Check that the value of 'r-1' is approximately 3.737
     cy.get('#r-1').should('have.value', '3.737');
 
-        // Click on the radio button 'rbA-1'
+    // -----------------
+    // Test 2: calculate the regular payment
+    // Click on the radio button 'rbA-1'
     cy.get('#rbA-1').click();
 
     // Enter 108000 in P0Display-1
     cy.get('#P0Display-1').clear().type('108000');
 
     // Enter 0 in r-1
+    // Enter 108000 in P0Display-1
     cy.get('#r-1').clear().type('0');
 
     // Click the "Calculate" button again
     cy.get('#search-btn-1').click();
 
     // Wait for calculations to complete
-    cy.wait(500);
+    cy.wait(300);
 
     // Check that the value of 'ADisplay-1' is approximately 900
     cy.get('#ADisplay-1').should('have.value', '900');
+
+    // -----------------
+    // Test 3: calculate the loan amount
+    cy.get('#rbP0-1').click();
+
+    // Enter 108000 in P0Display-1
+    cy.get('#ADisplay-1').clear().type('1000');
+    cy.get('#r-1').clear().type('4');
+
+    // Click the "Calculate" button again
+    cy.get('#search-btn-1').click();
+
+    // Wait for calculations to complete
+    cy.wait(300);
+
+    // Check that the value of 'ADisplay-1' is approximately 900
+    cy.get('#P0Display-1').should('have.value', '98770.17');
+
+    // -----------------
+    // Test 4: calculate the regular payment
+    //cy.visit('/cf');
+    cy.get('#rbT-1').click();
+
+    // Enter 108000 in P0Display-1
+    
+    cy.get('#ADisplay-1').clear().type('1000');
+    cy.get('#r-1').clear().type('4');
+    cy.get('#P0Display-1').clear().type('90000');
+
+    // Click the "Calculate" button again
+    cy.get('#search-btn-1').click();
+
+    // Wait for calculations to complete
+    cy.wait(400);
+
+    // Check that the value of 'ADisplay-1' is approximately 900
+    cy.get('#T_years-1').should('have.value', '9');
+    cy.get('#T_months-1').should('have.value', '0');
+
+    
     });
   });
   
